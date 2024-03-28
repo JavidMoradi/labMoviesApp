5b978efb516e5d365842712a853aaa86feedbeaf
Javid M <javidmoradi0@gmail.com>
2024-03-28 19:21:31 +0300

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
@@ -6,6 +6,8 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
 import { MovieT } from "../../types/interfaces";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { Avatar } from "@mui/material";
 
 const styles = {
     root: {
@@ -15,14 +17,37 @@ const styles = {
         flexWrap: "wrap",
         padding: 1.5,
     },
+    avatar: {
+        backgroundColor: "rgb(255, 0, 0)",
+    },
 };
 
 const MovieHeader: React.FC<MovieT> = (props) => {
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    useEffect(() => {
+        const items = JSON.parse(localStorage.getItem("favourites") ?? "[]");
+
+        if (items) {
+            items.forEach((e) => {
+                if (e.id == props.id) {
+                    setIsFavorite(true);
+                }
+            });
+        }
+    });
+
     return (
         <Paper component="div" sx={styles.root}>
             <IconButton aria-label="go back">
                 <ArrowBackIcon color="primary" fontSize="large" />
             </IconButton>
+
+            {isFavorite ? (
+                <Avatar sx={styles.avatar}>
+                    <FavoriteIcon />
+                </Avatar>
+            ) : null}
 
             <Typography variant="h4" component="h3">
                 {props.title}
