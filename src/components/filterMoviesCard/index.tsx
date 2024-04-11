6ef9f3227cab66e3f8b7fc2fbbc64e43c33ced58
Javid M <javidmoradi0@gmail.com>
2024-04-11 12:34:43 +0100

    Using react-query to manage the browser-based cache.

--- src/components/filterMoviesCard/index.tsx
+++ src/components/filterMoviesCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ChangeEvent } from "react";
+import React, { ChangeEvent } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -7,9 +7,11 @@ import MenuItem from "@mui/material/MenuItem";
 import TextField from "@mui/material/TextField";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import SortIcon from "@mui/icons-material/Sort";
-import { FilterOption } from "../../types/interfaces";
+import { FilterOption, GenreData } from "../../types/interfaces";
 import { SelectChangeEvent } from "@mui/material";
 import { getGenres } from "../../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from "../spinner";
 
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
@@ -34,14 +36,21 @@ interface FilterMoviesCardProps {
 }
 
 const FilterMoviesCard: React.FC = (props) => {
-    const [genres, setGenres] = useState([{ id: "0", name: "All" }]);
+    const { data, error, isLoading, isError } = useQuery<GenreData, Error>(
+        "genres",
+        getGenres
+    );
 
-    useEffect(() => {
-        getGenres().then((allGenres) => {
-            setGenres([genres[0], ...allGenres]);
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    if (isLoading) {
+        return <Spinner />;
+    }
+    if (isError) {
+        return <h1>{(error as Error).message}</h1>;
+    }
+    const genres = data?.genres || [];
+    if (genres[0].name !== "All") {
+        genres.unshift({ id: "0", name: "All" });
+    }
 
     const handleChange = (
         e: SelectChangeEvent,
